25c2f7dfc18e0180cdaa5b682d308af2903a5196
Lee Jieun <58466461+ammumal@users.noreply.github.com>
2020-09-21 19:57:18 +0900

    Update process_delete.php

--- W03-P/process_delete.php
+++ W03-P/process_delete.php
@@ -1,23 +1,22 @@
 <?php
   //연결
   $link = mysqli_connect("localhost", "root", "rootroot", "dbp");
-  settype($_GET['id'], "int");
+  settype($_POST['id'], "int");
   $filtered = array(
-    'id' => mysqli_real_escape_string($link, $_GET['id'])
+    'id' => mysqli_real_escape_string($link, $_POST['id'])
   );
   $query = "
     DELETE
       FROM topic
       WHERE
-        id = '{$filtered['id']}'
+        id = {$filtered['id']}
   ";
 
   $result = mysqli_multi_query($link, $query);
   //결과 출력
-  if($result == false) {
-    echo '삭제하는 과정에서 문제가 발생했습니다. 관리자에게 문의하세요.';
-    error_log(mysql_error($link));
+  if ($result == false) {
+      echo '삭제하는 과정에서 문제가 발생했습니다. 관리자에게 문의하세요.';
+      error_log(mysql_error($link));
   } else {
-    echo '성공했습니다. <a href="index.php">돌아가기</a>';
+      echo '성공했습니다. <a href="index.php">돌아가기</a>';
   }
- ?>
